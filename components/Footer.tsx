41131d275790cc0f094877a956de975cb95bff08
pankaj <imvpankaj@gmail.com>
2025-01-28 12:48:29 +0530

    asd

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,21 +1,34 @@
-"use client"
+"use client";
 
-import { motion } from "framer-motion"
-
+import { motion } from "framer-motion";
 
 export function Footer() {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-50 p-2">
-      <div className="container mx-auto flex justify-between items-center">
-      <motion.button
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
-          className="border-2 border-[#ff9800] text-[#ff9800] border-dashed hover:bg-[#ff9800]  hover:text-black px-4 py-3 rounded-lg text-2xl font-bold hover:bg-social-pink/10 transition-colors"
-        >
-          No More
-        </motion.button>
+    <>
+      <div className="flex justify-end">
+        <nav className="fixed bottom-0 left-0 right-0 z-50 p-2">
+          <div className="container mx-auto flex justify-between items-center">
+            <motion.button
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              className="border-2 border-[#ff9800] text-[#ff9800] border-dashed hover:bg-[#ff9800]  hover:text-black px-4 py-3 rounded-lg text-2xl font-bold hover:bg-social-pink/10 transition-colors"
+            >
+              No More
+            </motion.button>
+          </div>
+        </nav>{" "}
+        <nav className="fixed bottom-0 right-0 z-50 p-2">
+          <div className="container mx-auto flex justify-between items-center">
+            <motion.button
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              className="border-2 border-[#ff9800] text-[#ff9800] border-dashed hover:bg-[#ff9800]  hover:text-black px-4 py-3 rounded-lg text-2xl font-bold hover:bg-social-pink/10 transition-colors"
+            >
+              Quick Guide
+            </motion.button>
+          </div>
+        </nav>
       </div>
-    </nav>
-  )
+    </>
+  );
 }
-
