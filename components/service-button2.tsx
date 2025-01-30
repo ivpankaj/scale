c4fb48e627b16bbad38ea464d009fcef32eabdb2
pankaj <imvpankaj@gmail.com>
2025-01-30 17:40:10 +0530

    sad

--- components/service-button2.tsx
+++ components/service-button2.tsx
@@ -14,19 +14,20 @@ const ServiceButton2: React.FC<ServiceButton2Props> = ({
   onClick,
   expanded,
   staticData,
-
 }) => {
   return (
     <div className="relative w-full max-w-md lg:max-w-lg mx-auto">
- 
       <div className="w-full">
         <button
-        id="pk"
+          id="pk"
           onClick={onClick}
-          className="flex items-center justify-between w-[90vw] p-4 bg-[#202020] rounded-lg transition-colors box-border border border-transparent"
+          className={`flex items-center justify-between w-[90vw] p-4 bg-[#202020] rounded-lg transition-colors box-border border ${
+            expanded ? "border-[#ff9800] border-b-transparent" : "border-black"
+          }`}
         >
-    
-          <span className="flex justify-start text-left w-full">{children}</span>
+          <span className="flex justify-start text-left w-full">
+            {children}
+          </span>
 
           <span className="text-[#ff9800] text-xl font-bold">
             {expanded ? "-" : "+"}
@@ -40,7 +41,7 @@ const ServiceButton2: React.FC<ServiceButton2Props> = ({
               animate={{ opacity: 1, height: "auto" }}
               exit={{ opacity: 0, height: 0 }}
               transition={{ duration: 0.2 }}
-              className="w-full bg-[#393737] border border-[#ff9800] rounded-lg overflow-hidden box-border mt-2"
+              className="w-full bg-[#202020] border border-b-[#ff9800] border-l-[#ff9800]  border-r-[#ff9800] border-t-transparent rounded-lg overflow-hidden box-border"
             >
               <div className="p-4 w-[90vw]">
                 <span className="flex justify-start">{staticData}</span>
